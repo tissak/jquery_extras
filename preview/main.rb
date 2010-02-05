d5ddac447c3d4c27d0c6e88a1cb1977add874384
Tony Issakov <tissakov@thefrontiergroup.com.au>
2010-02-05 14:40:43 +0800

    revise dependency system

--- preview/main.rb
+++ preview/main.rb
@@ -4,3 +4,8 @@ require 'sinatra'
 get "/" do
   redirect "/index.html"
 end
+
+get "/javascript/dep/:name.js" do |name|
+  sleep 1
+  File.read("#{Dir.pwd}/public/javascript/dependency/#{name}.js")
+end
